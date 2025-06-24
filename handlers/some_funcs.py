8338c5aa8735eca16d7601736406d96cec9ae303
hosaph <torkinsergeu06@gmail.com>
2025-06-24 20:49:03 +0300

    version-2

--- handlers/some_funcs.py
+++ handlers/some_funcs.py
@@ -3,6 +3,8 @@ from fake_db.users import users_dict
 from random import randint
 
 def register_user(message: Message):
+    if message.from_user.id not in users_dict.keys():
+        users_dict[message.from_user.id] = [0]*4
     x = randint(1, 3)
     if x == 1:
         robot_move = "Камень"
@@ -10,4 +12,7 @@ def register_user(message: Message):
         robot_move = "Ножницы"
     elif x == 3:
         robot_move = "Бумага"    
-    users_dict[message.from_user.id] = robot_move
+    users_dict[message.from_user.id][3] = robot_move
+
+    
+
--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -12,7 +12,13 @@ router = Router()
 @router.message(Command("start"))
 async def start(message: Message):
     await message.answer(f"{LEXICON_RU["start"]}", reply_markup=keyboard_start)
-    
+@router.message(Command("help"))
+async def help(message: Message):
+    await message.answer(LEXICON_RU["help"])
+@router.message(Command("stat"))
+async def stat(message: Message):
+    if message.from_user.id in users_dict.keys():
+        await message.answer(f"Побед - {users_dict[message.from_user.id][0]}\nПоражений - {users_dict[message.from_user.id][1]}\nНичьих - {users_dict[message.from_user.id][2]}")
 @router.message(F.text == "Давай!")
 async def play(message: Message):
     await message.answer("Отлично!", reply_markup=ReplyKeyboardRemove())
@@ -21,7 +27,7 @@ async def play(message: Message):
 
 @router.message(F.text == "Че-то не хочу пока")
 async def no(message: Message):
-    if message.from_user.id in users_dict.keys() and users_dict[message.from_user.id]:
+    if message.from_user.id in users_dict.keys() and users_dict[message.from_user.id][3]:
         await message.answer("Доиграй сначала :)", reply_markup=keyboard_game)
     else:
         await message.answer("Хорошо. Как захочешь - пиши")
@@ -29,17 +35,20 @@ async def no(message: Message):
 async def in_game(message: Message):
     if message.from_user.id in users_dict.keys() and users_dict[message.from_user.id]:
         user_move = message.text
-        robot_move = users_dict[message.from_user.id]
+        robot_move = users_dict[message.from_user.id][3]
         if (user_move == "Камень" and robot_move == "Бумага" or 
             user_move == "Бумага" and robot_move == "Ножницы" or
             user_move == "Ножницы" and robot_move == "Бумага"):
-            users_dict[message.from_user.id] = None
+            users_dict[message.from_user.id][3] = None
+            users_dict[message.from_user.id][1] += 1
             await message.answer("Я выиграл! Сыграем еще?", reply_markup=keyboard_start)
         elif (user_move == "Камень" and robot_move == "Ножницы" or
               user_move == "Ножницы" and robot_move == "Бумага" or
               user_move == "Бумага" and robot_move == "Камень"):
-            users_dict[message.from_user.id] = None
+            users_dict[message.from_user.id][3] = None
+            users_dict[message.from_user.id][0] += 1
             await message.answer("Ты выиграл! Сыграем еще?", reply_markup=keyboard_start)
         elif user_move == robot_move:
-            users_dict[message.from_user.id] = None
+            users_dict[message.from_user.id][3] = None
+            users_dict[message.from_user.id][2] += 1
             await message.answer("Ничья! Сыграем еще?", reply_markup=keyboard_start)
