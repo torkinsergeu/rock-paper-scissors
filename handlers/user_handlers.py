e79b42fe02052021e8132014d5a6904f27707231
hosaph <torkinsergeu06@gmail.com>
2025-06-25 07:44:12 +0300

    some

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -12,18 +12,22 @@ router = Router()
 @router.message(Command("start"))
 async def start(message: Message):
     await message.answer(f"{LEXICON_RU["start"]}", reply_markup=keyboard_start)
+
 @router.message(Command("help"))
 async def help(message: Message):
     await message.answer(LEXICON_RU["help"])
+
 @router.message(Command("stat"))
 async def stat(message: Message):
     if message.from_user.id in users_dict.keys():
         await message.answer(f"Побед - {users_dict[message.from_user.id][0]}\nПоражений - {users_dict[message.from_user.id][1]}\nНичьих - {users_dict[message.from_user.id][2]}")
+
 @router.message(F.text == "Давай!")
 async def play(message: Message):
     await message.answer("Отлично!", reply_markup=ReplyKeyboardRemove())
     register_user(message)
     await message.answer("Я загадал!", reply_markup=keyboard_game)    
+
 @router.message(F.text == "Че-то не хочу пока")
 async def no(message: Message):
     if message.from_user.id in users_dict.keys() and users_dict[message.from_user.id][3]:
@@ -37,18 +41,23 @@ async def in_game(message: Message):
     if message.from_user.id in users_dict.keys() and users_dict[message.from_user.id]:
         user_move = message.text
         robot_move = users_dict[message.from_user.id][3]
+        
         if (user_move == "Камень" and robot_move == "Бумага" or 
             user_move == "Бумага" and robot_move == "Ножницы" or
             user_move == "Ножницы" and robot_move == "Камень"):
+        
             users_dict[message.from_user.id][3] = None
             users_dict[message.from_user.id][1] += 1
             await message.answer("Я выиграл! Сыграем еще?", reply_markup=keyboard_start)
+        
         elif (user_move == "Камень" and robot_move == "Ножницы" or
               user_move == "Ножницы" and robot_move == "Бумага" or
               user_move == "Бумага" and robot_move == "Камень"):
+        
             users_dict[message.from_user.id][3] = None
             users_dict[message.from_user.id][0] += 1
             await message.answer("Ты выиграл! Сыграем еще?", reply_markup=keyboard_start)
+        
         elif user_move == robot_move:
             users_dict[message.from_user.id][3] = None
             users_dict[message.from_user.id][2] += 1
